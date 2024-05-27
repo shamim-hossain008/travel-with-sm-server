fe1bf5c918167570ad0d87ea3b1c52ba8daa1cd8
shamim-hossain008 <shamim.hossain3573@gmail.com>
2024-05-28 01:00:31 +0800

    get app added done  for view details data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5010;
@@ -24,6 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const userCollection = client.db("userDB").collection("users");
+    const spotCollection = client.db("travelWithSm").collection("spots");
 
     app.post("/user", async (req, res) => {
       try {
@@ -39,6 +40,42 @@ async function run() {
       const query = {};
       const user = await userCollection.find(query).toArray();
       res.send(user);
+    });
+
+    // post addList data
+    app.post("/addList", async (req, res) => {
+      try {
+        const newAdd = req.body;
+        console.log(newAdd);
+        const result = await spotCollection.insertOne(newAdd);
+        res.send(result);
+      } catch {
+        console.error(error);
+      }
+    });
+
+    // Get for My list
+    app.get("/myListSpot/:email", async (req, res) => {
+      try {
+        const listSpot = req.params.email;
+        console.log(listSpot);
+        const result = await spotCollection.find({ email: listSpot }).toArray();
+        res.send(result);
+      } catch {
+        console.error(error);
+      }
+    });
+
+    // get for view details
+    app.get("/viewSpot/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await spotCollection.findOne(query);
+        res.send(result);
+      } catch {
+        console.error(error);
+      }
     });
   } finally {
   }
